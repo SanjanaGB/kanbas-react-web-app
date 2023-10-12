31bcabb1842dc9983fc3f3acd4e28d367ca7ac1e
sanjanagb <sanjana051198@gmail.com>
2023-10-12 14:32:13 -0400

    Assignment 3

---
+++ src/Labs/a3/index.js
@@ -0,0 +1,42 @@
+import JavaScript from "./JavaScript";
+import PathParameters from "./PathParameters";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
+function Assignment3() {
+    return (
+        <div>
+            <h1>Assignment 3</h1>
+            <TodoList />
+            <hr />
+            <ul className="list-group">
+                <TodoItem
+                    todo={{
+                        title: "Buy milk",
+                        done: true,
+                        status: "COMPLETE",
+                    }}
+                />
+                <TodoItem
+                    todo={{
+                        title: "Pick up kids",
+                        done: false,
+                        status: "IN_PROGRESS",
+                    }}
+                />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </ul>
+            <TodoItem />
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+            <PathParameters/>
+            <JavaScript/>
+        </div>
+    );
+}
+export default Assignment3;
